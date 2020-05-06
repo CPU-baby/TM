09e8953aa176159156d3d8b9f65795375930c3c1
yeLyn36 <s2018w11@e-mirim.hs.kr>
2020-05-07 02:33:34 +0900

    랭킹 데이터베이스 추가

---
+++ Assets/Scripts/PressEnterkey.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PressEnterkey : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed.");
+            SceneManager.LoadScene("OnestageScene");
+        }
+
+        //if (Input.GetKeyUp(KeyCode.KeypadEnter))
+        //    Debug.Log("Enter key was released.");
+    }
+
+
+}
