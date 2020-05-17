39d97e7551ef4be3d452c2ea510f25115e3576fa
ovoHJ <s2018w01@gmail.com>
2020-05-18 01:19:38 +0900

    save InputField

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputManager : MonoBehaviour
+{
+    public InputField inputName;
+    public InputField inputNum;
+
+    public void checkName()
+    {
+        Debug.Log(inputName.text);
+    }
+    public void checkNum()
+    {
+        Debug.Log(inputName.text);
+    }
+
+     void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            if(inputName.text.Equals(""))
+            {
+                Debug.Log("Empty name");
+            }
+            else if (inputNum.text.Equals(""))
+            {
+                Debug.Log("Empty num");
+            }
+            else
+            {
+                // 정보 저장하는거
+                PlayerPrefs.SetString("name", inputName.text);
+                PlayerPrefs.SetString("num", inputNum.text);
+
+                /* 이게 정보 가져오는 거
+                string dName = PlayerPrefs.GetString("name");
+                string dNum = PlayerPrefs.GetString("num");
+
+
+                Debug.Log("dName is " + dName);
+                Debug.Log("dNum is " + dNum);
+                */
+                Debug.Log("Enter key was pressed.");
+                SceneManager.LoadScene("WayScene");
+            }
+        }
+
+    }
+}
